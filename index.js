97001d8b78f11393b4eb4793e60f0e423d192c37
ItsMeBrozy <barzanazizwork@gmail.com>
2026-05-05 14:21:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ if (!TOKEN) {
   process.exit(1);
 }
 
+// DEBUG: Log masked token to verify which one Railway is using
+console.log(`Attempting to login with token starting with ${TOKEN.substring(0, 4)}... and ending with ...${TOKEN.substring(TOKEN.length - 4)}`);
+
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
@@ -21,4 +24,12 @@ client.on('error', (err) => {
   console.error('Discord client error:', err);
 });
 
-client.login(TOKEN);
+client.login(TOKEN).catch(err => {
+  console.error('Failed to login with provided token:', err);
+  process.exit(1);
+});
+
+// Global handler for unhandled promise rejections to aid debugging in deployment environments
+process.on('unhandledRejection', (reason, p) => {
+  console.error('Unhandled Rejection at:', p, 'reason:', reason);
+});
